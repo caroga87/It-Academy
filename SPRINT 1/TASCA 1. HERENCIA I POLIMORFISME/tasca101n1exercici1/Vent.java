6c18d3c3707f86ab5d775b219f9cd7a2b22f120b
Carla Roca <159011993+caroga87@users.noreply.github.com>
2024-02-27 16:28:56 +0100

    Update Vent.java

--- SPRINT 1/TASCA 1. HERENCIA I POLIMORFISME/tasca101n1exercici1/Vent.java
+++ SPRINT 1/TASCA 1. HERENCIA I POLIMORFISME/tasca101n1exercici1/Vent.java
@@ -6,7 +6,7 @@ public class Vent extends Instruments {
 	public Vent (String nom, double preu) {
 		super(nom, preu);
 		}
-	
+	@Override
 	public void tocar () {
 		System.out.println("Està sonant un instrument de vent");
 	}
