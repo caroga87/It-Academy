5e84804a74e6c043963419ec331d3b6bc4d9cff4
Carla Roca <159011993+caroga87@users.noreply.github.com>
2024-02-27 16:28:42 +0100

    Update Percussio.java

--- SPRINT 1/TASCA 1. HERENCIA I POLIMORFISME/tasca101n1exercici1/Percussio.java
+++ SPRINT 1/TASCA 1. HERENCIA I POLIMORFISME/tasca101n1exercici1/Percussio.java
@@ -5,7 +5,7 @@ public class Percussio extends Instruments {
 	public Percussio (String nom, double preu) {
 		super(nom, preu);
 	}
-	
+	@Override
 	public void tocar () {
 		System.out.println("Està sonant un instrument de precussió");
 	}
