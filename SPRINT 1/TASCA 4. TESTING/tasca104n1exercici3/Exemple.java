170cbd6541217757ecd79e944eb86973cc4841ff
Carla Roca <159011993+caroga87@users.noreply.github.com>
2024-02-28 10:54:09 +0100

    Update Exemple.java

--- SPRINT 1/TASCA 4. TESTING/tasca104n1exercici3/Exemple.java
+++ SPRINT 1/TASCA 4. TESTING/tasca104n1exercici3/Exemple.java
@@ -1,15 +1,36 @@
 package tasca104n1exercici3;
 
+import java.util.ArrayList;
+
 //Crea una classe amb un mètode que llanci una ArrayIndexOutOfBoundsException.
 //Verifica el seu correcte funcionament amb un test jUnit.
-
-
 public class Exemple {
 	  
-	public void accesArray () {
-		 int[] array = new int[5];
-	        // Intentamos acceder a un índice que está fuera del tamaño del array
-	        int valor = array[10];
+	  public static ArrayList<String> crearLlistaMesos() {
+	    	ArrayList<String> mesos = new ArrayList<>();
+	        mesos.add("gener");
+	        mesos.add("febrer");
+	        mesos.add("març");
+	        mesos.add("abril");
+	        mesos.add("maig");
+	        mesos.add("juny");
+	        mesos.add("juliol");
+	        mesos.add("agost");
+	        mesos.add("setembre");
+	        mesos.add("octubre");
+	        mesos.add("novembre");
+	        mesos.add("desembre");
+	                       
+	        return mesos;
 	    }
+	  
+	  public static String obtenirMes(int index) {
+	        ArrayList<String> mesos = crearLlistaMesos();
+	      
+	        if (index <= mesos.size()) {
+	        	return mesos.get(index);
+	        }else {
+	    	throw new ArrayIndexOutOfBoundsException ();
+	    }
+	  }
 }
-	
