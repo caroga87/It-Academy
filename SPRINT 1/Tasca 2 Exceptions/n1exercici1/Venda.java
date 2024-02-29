398b005be77b3bfcf0ece1c7bfaaac452bed7793
Carla Roca <159011993+caroga87@users.noreply.github.com>
2024-02-29 11:44:59 +0100

    Update Venda.java
    Correccions

--- SPRINT 1/Tasca 2 Exceptions/n1exercici1/Venda.java
+++ SPRINT 1/Tasca 2 Exceptions/n1exercici1/Venda.java
@@ -3,59 +3,61 @@ package n1exercici1;
 import java.util.ArrayList;
 
 public class Venda {
-	//atributs
-	private ArrayList<Producte> llistaProductes;
-	private double preuTotal;
 	
-	//constructor
-	public Venda (double preuTotal) {
-		llistaProductes=new ArrayList<Producte>();
-		this.preuTotal=preuTotal;
-		
-		}
-	//getters i setters
-	public double getPreuTotal() {
-		return preuTotal;
-	}
+private ArrayList<Producte> llistaProductes;
+private double preuTotal;
+
+
+public Venda (double preuTotal) {
+	llistaProductes=new ArrayList<Producte>();
+	this.preuTotal=preuTotal;
 	
-	public ArrayList<Producte> getLlistaProductes (){
-		return llistaProductes;
 	}
 
-	public void setPreuTotal(double preuTotal) {
-		this.preuTotal = preuTotal;
-	}
+public double getPreuTotal() {
+	return preuTotal;
+}
+
+public ArrayList<Producte> getLlistaProductes (){
+	return llistaProductes;
+}
+
+public void setPreuTotal(double preuTotal) {
+	this.preuTotal = preuTotal;
+}
+
+
+
+public void afegirProducte(Producte producte) {
 	
-	//metodes de classe
-	
-	public ArrayList<Producte> afegirProducte(Producte producte) {
-		llistaProductes.add(producte);
-		return llistaProductes;
-	}
-	public void calcularTotal() throws VendaBuidaException {
-		
-		if (llistaProductes.isEmpty()) {
-			
-			throw new VendaBuidaException();
-		
-		} else { 
-			for(Producte p: llistaProductes) {
-				preuTotal+=p.getPreu();
-			}
-		System.out.println("Preu total de la venda " +preuTotal);
-			}
-	}
-	
-	public void mostrarProducte(int index) throws ArrayIndexOutOfBoundsException {
-		
-		if (llistaProductes.size()< index) {
-			throw new ArrayIndexOutOfBoundsException ();
-		}else {
-			System.out.println(llistaProductes.get(index));
-		}
-   }
+	llistaProductes.add(producte);
 }
 	
+public void calcularTotal() throws VendaBuidaException {
+	
+	if (llistaProductes.isEmpty()) {
+		
+		throw new VendaBuidaException();
+	}
+	
+	for(Producte p: llistaProductes) {
+		preuTotal+=p.getPreu();
+		
+	System.out.println("Preu total de la venda " +preuTotal);
+		}
+}
+
+public void mostrarProducte(int index) throws ArrayIndexOutOfBoundsException {
+	
+	if (llistaProductes.size()< index) {
+		throw new ArrayIndexOutOfBoundsException ();
+	}
+		System.out.println(llistaProductes.get(index));
+	
+}
+}
+	
+
 
 
 
