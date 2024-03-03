e31885c1bee34fa50e4859f684acdf3f361dc768
Carla Roca <159011993+caroga87@users.noreply.github.com>
2024-03-03 22:27:27 +0100

    Update Llista.java

--- SPRINT 1/Tasca 3 Java Collections/n1exercici2/Llista.java
+++ SPRINT 1/Tasca 3 Java Collections/n1exercici2/Llista.java
@@ -6,7 +6,7 @@ import java.util.ListIterator;
 
 public class Llista {
     public static void main(String[] args) {
-        // Creació i emplenament de la primera llista
+       
         List<Integer> llista1 = new ArrayList<>();
         llista1.add(1);
         llista1.add(2);
@@ -14,18 +14,15 @@ public class Llista {
         llista1.add(4);
         llista1.add(5);
         
-        // Creació de la segona llista
+       
         List<Integer> llista2 = new ArrayList<>();
-        
-        
-        // Utilitzant un ListIterator per llegir els elements de la primera llista i inserir-los en la segona
+       
         ListIterator<Integer> iterator = llista1.listIterator(llista1.size());
         while (iterator.hasPrevious()) {
             Integer element = iterator.previous();
             llista2.add(element);
         }
         
-        // Mostrem la segona llista amb els elements inserits en ordre invers
         System.out.println("Segona llista amb elements de la primera en ordre invers:");
         for (Integer num : llista2) {
             System.out.println(num);
